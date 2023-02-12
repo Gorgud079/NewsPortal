b1d7885690438d07b6219feafd10140c1cbec45c
Gorgud079 <des079@inbox.ru>
2023-02-12 23:15:42 +0400

    correction censor (check function, fix cod when for example - (арсенал)), add content and headline of the post1

--- Portal/NewsPortal/templatetags/custom_filters.py
+++ Portal/NewsPortal/templatetags/custom_filters.py
@@ -1,5 +1,5 @@
 from django import template
-from ..example import SPISOK
+from ..example import SPISOK, PUN
 
 
 register = template.Library()
@@ -8,14 +8,37 @@ def censor(value):
         value = list(map(str, value.split()))
         content = []
         for word in value:
-            if (word.lower()) in SPISOK:
-                le = len(word) - 2
-                word = f"{word[0]}{'*'*le}{word[-1]}"
-                content.append(word)
-                continue
+            print(word)
+            if word.startswith(PUN) and word.endswith(PUN):
+                print(word.startswith(PUN))
+                print(word.endswith(PUN))
+                if (word.lower()[1:-1]) in SPISOK:
+                    le = len(word) - 4
+                    word = f"{word[0]}{word[1]}{'*' * le}{word[-2]}{word[-1]}"
+                    content.append(word)
+                else:
+                    content.append(word)
+            elif word.startswith(PUN):
+                if (word.lower()[1:-1]) in SPISOK:
+                    le = len(word) - 3
+                    word = f"{word[1]}{'*' * le}{word[-1]}"
+                    content.append(word)
+                else:
+                    content.append(word)
+            elif word.endswith(PUN):
+                if (word.lower()[0:-1]) in SPISOK:
+                    le = len(word) - 3
+                    word = f"{word[0]}{'*' * le}{word[-2]}{word[-1]}"
+                    content.append(word)
+                else:
+                    content.append(word)
             else:
-                content.append(word)
-
+                if (word.lower()) in SPISOK:
+                    le = len(word) - 2
+                    word = f"{word[0]}{'*' * le}{word[-1]}"
+                    content.append(word)
+                else:
+                    content.append(word)
         return f"{(' '.join(content))}"
     #
 
