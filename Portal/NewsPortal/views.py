38c6772471980d04e757a383915a0465b0e5db6b
Gorgud079 <des079@inbox.ru>
2023-02-21 17:50:58 +0400

    added {%if "news" in request.path %} block in news-create.html

--- Portal/NewsPortal/views.py
+++ Portal/NewsPortal/views.py
@@ -4,6 +4,8 @@ from .models import Post, Comment
 from .filters import PostFilter
 from .forms import PostForm
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.views.generic import TemplateView
 from datetime import datetime
 # Create your views here.
 class PostsList(ListView):
@@ -45,15 +47,18 @@ class PostCreate(CreateView):
     template_name = 'news_create.html'
     permission_required = ('news.add_post',)
 
+
     def form_valid(self, form):
         post = form.save(commit=False)
         if 'news' in self.request.path.split('/'):
             post.position = "NS"
+            self.template_name = 'news_create.html'
             self.success_url = reverse_lazy('news_create')
         else:
             post.position = "AE"
+            self.template_name = 'art_create.html'
             self.success_url = reverse_lazy('art_create')
-        return super().form_valid(form)
+        return super().form_valid(form), self.template_name
 
 
 class PostUpdate(UpdateView):
